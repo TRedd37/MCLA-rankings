fe758c70bc4683e7ccb4556007f981ede6f2b93e
Taylor Redd <redd@adobe.com>
2018-02-25 22:36:42 -0700

    Removed unneeded script

--- rankings_script.R
+++ rankings_script.R
@@ -1,55 +1,26 @@
-
-raw_results <- read.csv("~/Personal projects/MCLA-rankings/MCLA scores.csv", stringsAsFactors = FALSE)
-
-results <- raw_results[, c("Home.Team", "Away.Team")]
-results$Winning.Team <- "Home"
-results$Winning.Team[raw_results$Away.Score > raw_results$Home.Score] <- "Visiting"
-results$Neutral <- raw_results$N.CT.NT != ""
-
-
-raw_results_2018 <- read.csv("~/Personal projects/MCLA-rankings/2018 scores.csv", 
-                        stringsAsFactors = FALSE)
-
-results <- data.frame(Home.Team = raw_results_2018$Home,
-                      Away.Team = raw_results_2018$Away,
-                      Neutral = FALSE,
-                      stringsAsFactors = FALSE)
-results$Winning.Team <- "Home"
-results$Winning.Team[raw_results_2018$Score.Away > raw_results_2018$Score.Home] <- "Visiting"
-
-
-raw_results_2018 <- read.csv("~/Personal projects/MCLA-rankings/2018 scores 2018_02_13.csv", 
-                             stringsAsFactors = FALSE)
-
-results <- data.frame(Home.Team = raw_results_2018$Home,
-                      Away.Team = raw_results_2018$Away,
-                      Neutral = FALSE,
-                      stringsAsFactors = FALSE)
-results$Winning.Team <- "Home"
-results$Winning.Team[raw_results_2018$Away.Score > raw_results_2018$Home.Score] <- "Visiting"
-
-
-
-# results <- data.frame(
-# 	Home.Team = c(1, 2, 3, 1, 2, 3),
-# 	Visiting.Team = c(3, 3, 1, 2, 1, 2), 
-# 	Winning.Team = c("Home", "Visiting", "Visiting", "Home", "Visiting", "Visiting"),
-# 	stringsAsFactors = FALSE)
-
-
-################################
-
-source("http://grimshawville.byu.edu/makedataMCLA2018.R")
+source("../../../../Personal projects/MCLA-rankings/data_pull.R")
+source("../../../../Personal projects/MCLA-rankings/Graves_Reese_rankings.R")
 
 head(mcla2018todate)
 
 results <- getFinishedResults(mcla2018todate)
 
-subset(results, Home.Team == "Boise State"|Away.Team == "Boise State")
+subset(results, Home.Team == "Clemson"|Away.Team == "Texas A&M")
 
 output <- calculateRankings(results, 10000)
-predictGameOutcome( "California","Brigham Young", output)
-extractRankings(output)
+predictGameOutcome("Boise State", "Brigham Young", output)
+1- predictGameOutcome("Stanford", "Brigham Young", output)
+1- predictGameOutcome("California", "Brigham Young", output)
+
+rankings <- data.frame(School = names(extractRankings(output)),
+                       Score = extractRankings(output),
+                       Rank = 1:length(extractRankings(output)),
+                       stringsAsFactors = FALSE)
+
+
+team_list <- as.list(sort(rankings$School))
+names(team_list) <- sort(rankings$School)
+
 
 extractRankings(output)[c("California", "Brigham Young", "UNLV", "Boise State", "Utah")]
 
