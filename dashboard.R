32fe0fe1df5a8dad86c9de2a7d556ae06cec4d8b
Taylor Redd <redd@adobe.com>
2018-02-25 22:35:44 -0700

    Dashboard creation

---
+++ dashboard.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Redd's Rankings"),
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Rankings",  dataTableOutput("rankings") ),
+      tabPanel("Predictions", 
+               selectInput("homeTeam", 
+                           label = "Select Home Team:", 
+                           choices = team_list),
+               selectInput("awayTeam", 
+                           label = ("Select Away Team:"), 
+                           choices = team_list),
+               textOutput("prediction")),
+    tabPanel("Games", dataTableOutput("games"))
+    )
+  )
+)
+
+server <- function(input, output) {
+
+  output$rankings   <- renderDataTable({rankings})
+  output$games      <- renderDataTable({results})
+  output$prediction <- renderText(predictGameOutcome(input$homeTeam, 
+                                                      input$awayTeam, 
+                                                      model_output))
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
