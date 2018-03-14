616dd6ae5f280a23527872577c6a40ad39c98703
Taylor Redd <redd@adobe.com>
2018-03-13 21:43:12 -0600

    small changes

--- rankings_script.R
+++ rankings_script.R
@@ -12,12 +12,10 @@ rankings <- data.frame(School = names(extractRankings(output)),
                        Score  = extractRankings(output),
                        Rank   = 1:length(extractRankings(output)),
                        stringsAsFactors = FALSE)
+record   <- ldply(rankings$School,  getRecord, results = results)
+rankings <- cbind(rankings, record)
 
-record <- ldply(rankings$School,  getRecord, results = results)
-
-
-cbind(rankings, record)
-
+subset(results, Home.Team == "Brigham Young" | Away.Team == "Brigham Young")
 
 which(rankings$School == "Brigham Young")
 
@@ -26,6 +24,8 @@ which(rankings$School == "Brigham Young")
 
 netteam_list <- as.list(sort(rankings$School))
 names(team_list) <- sort(rankings$School)
+
+save(results, rankings, model_output, file = "../../../../Personal projects/MCLA-rankings/backup.RData")
 
 
 
