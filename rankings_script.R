0635dfdc8560d13d07ae4c26a7acea1d81cf73da
Taylor Redd <redd@adobe.com>
2018-03-14 10:34:27 -0600

    adding Dvision to output

--- rankings_script.R
+++ rankings_script.R
@@ -6,14 +6,16 @@ mcla2018todate$date <- parse_date_time(mcla2018todate$Date, "a b d")
 results <- getFinishedResults(mcla2018todate)
 
 
-output <- calculateRankings(results, 5000)
+model_output <- calculateRankings(results, 5000)
 
 rankings <- data.frame(School = names(extractRankings(output)),
                        Score  = extractRankings(output),
                        Rank   = 1:length(extractRankings(output)),
+                       Division = 1,
                        stringsAsFactors = FALSE)
 record   <- ldply(rankings$School,  getRecord, results = results)
 rankings <- cbind(rankings, record)
+rankings$Division[rankings$School %in% d2teams] <- 2
 
 subset(results, Home.Team == "Brigham Young" | Away.Team == "Brigham Young")
 
@@ -26,7 +28,6 @@ netteam_list <- as.list(sort(rankings$School))
 names(team_list) <- sort(rankings$School)
 
 save(results, rankings, model_output, file = "../../../../Personal projects/MCLA-rankings/backup.RData")
-
 
 
 
