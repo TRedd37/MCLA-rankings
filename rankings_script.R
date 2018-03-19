2546bca4666bde255495dba3657dd7cd426f2715
Taylor Redd <redd@adobe.com>
2018-03-19 11:00:45 -0600

    cleaning up script

--- rankings_script.R
+++ rankings_script.R
@@ -1,13 +1,12 @@
-source("../../../../Personal projects/MCLA-rankings/data_pull.R")
+source("data_pull.R")
 #source("http://grimshawville.byu.edu/makedataMCLA2018.R")
-source("../../../../Personal projects/MCLA-rankings/Graves_Reese_rankings.R")
+source("Graves_Reese_rankings.R")
 
 mcla2018todate$date <- parse_date_time(mcla2018todate$Date, "a b d")
 
 results <- getFinishedResults(mcla2018todate)
 
-
-model_output <- calculateRankings(results, 5000)
+model_output <- calculateRankings(results, 50000)
 
 rankings <- data.frame(School = names(extractRankings(model_output)),
                        Score  = extractRankings(model_output),
@@ -20,7 +19,7 @@ rankings$Division[rankings$School %in% d2teams] <- 2
 
 which(rankings$School == "Brigham Young")
 
-1-predictGameOutcome("Utah", "Brigham Young", output)
+1-predictGameOutcome("Utah", "Brigham Young", model_output)
 
 save(results, rankings, model_output, file = "../../../../Personal projects/MCLA-rankings/backup.RData")
 
