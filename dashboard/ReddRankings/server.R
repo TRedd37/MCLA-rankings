56bc85bd4b83f49a6220f1474de06a2a69b97dfb
Taylor Redd <redd@adobe.com>
2019-03-21 22:10:31 -0600

    Allowing user to filter by date

--- dashboard/ReddRankings/server.R
+++ dashboard/ReddRankings/server.R
@@ -3,6 +3,7 @@ library(pool)
 library(RMySQL)
 library(dplyr)
 library(tidyr)
+library(lubridate)
 
 pool <- dbPool(
   drv = RMySQL::MySQL(),
@@ -12,37 +13,50 @@ pool <- dbPool(
   host = "reddrankings.cvjutlbtujzn.us-east-2.rds.amazonaws.com"
 )
 
-max_timestamp <- pool %>%
-  tbl("ModelResults") %>%
-  group_by(ModelID) %>%
-  summarize(maxTimestamp = max(Time, na.rm = TRUE))
-
-rankings_long <- pool %>%
-  tbl("ModelResults") %>%
-  left_join(max_timestamp, by = "ModelID") %>%
-  left_join(tbl(pool, "ModelIDs"), by = c(ModelID = "ID")) %>%
-  left_join(tbl(pool, "TeamIDs"), by = c(TeamID = "ID")) %>%
-  filter(Active == 1) %>%
-  filter(TIME == maxTimestamp) %>%
-  select(TeamName, ModelRank, ModelScore, ModelName) %>%
-  collect() %>%
-  group_by(TeamName) %>%
-  mutate(Average = mean(ModelRank),
-         AveScore = mean(ModelScore)) %>%
-  ungroup() %>%
-  select(-ModelScore) %>%
-  spread(ModelName, ModelRank) %>%
-  arrange(Average, desc(AveScore)) %>%
-  mutate(ReddRanking = 1:nrow(.)) %>%
-  select(-AveScore) %>%
-  rename(RR_v1 = "absolute_HFA")%>%
-  select(TeamName, ReddRanking, RR_v1, everything(), -Average, Average) 
-
 onStop(function() {
   poolClose(pool)
 })
 
 
 shinyServer(function(input, output) {
-  output$rankings   <- renderDataTable({rankings_long})
+  
+  input_time <- reactive({as.character(ymd(input$filterDate, 
+                              tz = "America/Denver") + hours(8))})
+
+  max_timestamp <- reactive({
+    pool %>%
+      tbl("ModelResults") %>%
+      group_by(ModelID) %>%
+      filter(Time < !!input_time()) %>%
+      summarize(maxTimestamp = max(Time, na.rm = TRUE))})
+
+  rankings_long <- reactive({
+    validate(
+      need(input$filterDate > ymd("2019-03-17"), 
+           "No rankings before 2019-03-18. Select another date")
+    )
+    
+    pool %>%
+      tbl("ModelResults") %>%
+      left_join(max_timestamp(), by = "ModelID") %>%
+      left_join(tbl(pool, "ModelIDs"), by = c(ModelID = "ID")) %>%
+      left_join(tbl(pool, "TeamIDs"), by = c(TeamID = "ID")) %>%
+      filter(Active == 1) %>%
+      filter(TeamName != "HFA") %>%
+      filter(TIME == maxTimestamp) %>%
+      select(TeamName, ModelRank, ModelScore, ModelName) %>%
+      collect() %>%
+      group_by(TeamName) %>%
+      mutate(Average = mean(ModelRank),
+             AveScore = mean(ModelScore)) %>%
+      ungroup() %>%
+      select(-ModelScore) %>%
+      spread(ModelName, ModelRank) %>%
+      arrange(Average, desc(AveScore)) %>%
+      mutate(ReddRanking = 1:nrow(.)) %>%
+      select(-AveScore) %>%
+      rename(RR_v1 = "absolute_HFA")%>%
+      select(TeamName, ReddRanking, RR_v1, everything(), -Average, Average)
+  })
+  output$rankings   <- renderDataTable({rankings_long()})
 })
--- dashboard/ReddRankings/ui.R
+++ dashboard/ReddRankings/ui.R
@@ -1,10 +1,12 @@
 library(shiny)
+library(lubridate)
 
 shinyUI(fluidPage(
   titlePanel("Redd Rankings"),
   mainPanel(
+    dateInput("filterDate", "Rankings as of:", value = today(tz = "America/Denver")),
     tabsetPanel(
-      tabPanel("Rankings",  dataTableOutput("rankings") )
+      tabPanel("Rankings", dataTableOutput("rankings") )
     )
   )
 ))
