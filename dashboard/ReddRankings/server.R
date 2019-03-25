9cc080b56a1415d0bcf95ec7142c7849e177a453
Taylor Redd <redd@adobe.com>
2019-03-25 10:22:59 -0600

    creating global.R file

--- dashboard/ReddRankings/server.R
+++ dashboard/ReddRankings/server.R
@@ -1,17 +1,4 @@
-library(shiny)
-library(pool)
-library(RMySQL)
-library(dplyr)
-library(tidyr)
-library(lubridate)
 
-pool <- dbPool(
-  drv = RMySQL::MySQL(),
-  dbname = "reddrankings",
-  username = "shiny",
-  password = "guest",
-  host = "reddrankings.cvjutlbtujzn.us-east-2.rds.amazonaws.com"
-)
 
 onStop(function() {
   poolClose(pool)
--- dashboard/ReddRankings/ui.R
+++ dashboard/ReddRankings/ui.R
@@ -1,12 +1,14 @@
-library(shiny)
-library(lubridate)
 
 shinyUI(fluidPage(
   titlePanel("Redd Rankings"),
   mainPanel(
     dateInput("filterDate", "Rankings as of:", value = today(tz = "America/Denver")),
+    checkboxGroupInput("conferences", "Conferences:", choices = all_conferences$Conference,
+                       selected = all_conferences$Conference, inline = TRUE),
+    checkboxGroupInput("divisions", "Divisions:", choices = c(1, 2),
+                       selected = 1:2, inline = TRUE),
     tabsetPanel(
-      tabPanel("Rankings", dataTableOutput("rankings") )
+      tabPanel("Rankings", DTOutput("rankings") ),
     )
   )
 ))
