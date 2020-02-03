7988027a3fc993cecd8c34c1067edc96a69ad7a5
Taylor <tredd37@gmail.com>
2020-02-03 14:23:09 -0700

    Using RMariaDB

--- cron/config.R
+++ cron/config.R
@@ -1,12 +1,13 @@
-library(RMySQL, quietly = TRUE)
 library(ReddRankings)
+library(DBI)
 
 iterations <- 50000
 
-results <- getResults("2019")
+results <- getResults("2020")
 
-con <- dbConnect(MySQL(),
+con <- dbConnect(RMariaDB::MariaDB(),
                  "reddrankings",
                  username = "tredd",
                  password = Sys.getenv("RDS_PASSWORD"),
                  host = "reddrankings.cvjutlbtujzn.us-east-2.rds.amazonaws.com")
+
