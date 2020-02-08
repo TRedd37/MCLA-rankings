813e5dd3ad29042b7674c06f06c9b20d67779f68
Taylor <tredd37@gmail.com>
2020-02-08 11:06:06 -0700

    creating new model update process
    Using multithreading instead of calling multiple R instances

---
+++ cron/daily_run.R
@@ -0,0 +1,21 @@
+library(pool)
+suppressMessages(library("doFuture"))
+library(ReddRankings)
+
+registerDoFuture()  ## tells foreach futures should be used
+plan(multisession)  ## specifies what type of futures
+iterations <-  50000
+
+results <- getResults("2020")
+
+model_options <- list(WF_method = c('logit', 'logit', 'step', 'step', 'absolute', 'absolute'),
+                      HFA = c(F,T,F,T,F,T))
+
+output <- foreach(i = 1:6, .packages = c("ReddRankings", "tidyverse", "dplyr")) %dopar% {
+  calculateRankings(results = results, iters = iterations, WF_method = model_options$WF_method[i], HFA = model_options$HFA[i])
+}
+names(output) <- c("logit", "logit_HFA", "step", "step_HFA", "absolute", "absolute_HFA")
+output[["scoreBased"]] <- leastSquaresRankings(results, FALSE)
+output[["scoreBased_HFA"]] <- leastSquaresRankings(results, TRUE)
+
+writeResultsToDatabase(output)
--- cron/rankings.sh
+++ cron/rankings.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/Rscript absolute.R &
-/usr/local/bin/Rscript least_squares.R &
-/usr/local/bin/Rscript logit.R &
-/usr/local/bin/Rscript step.R &
+/usr/local/bin/Rscript daily_run.R
